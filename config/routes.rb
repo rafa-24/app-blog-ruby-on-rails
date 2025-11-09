fc71111852f815241b0a1498dd033ebdd3c7ed86
rafa-24 <rafaprieto063@gmail.com>
2025-11-09 15:33:06 -0500

    feature/7.1-show-one-article

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get "articles", to: "articles#index"
 
+  get "articles/:id", to:"articles#show"
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
