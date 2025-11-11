b1ab472f08ab1727f79742f25a09f1b741437fef
rafa-24 <rafaprieto063@gmail.com>
2025-11-11 18:41:00 -0500

    feat: crud for articles in my blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,45 @@ class ArticlesController < ApplicationController
   def show 
     @article = Article.find(params[:id])
   end
+
+  def new #app/view/articles/new.html.erb
+    @article = Article.new
+  end
+
+  def create 
+    @article = Article.new(articles_params) 
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity # es un codigo de estado http
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id]) # Obtener un articulo
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(articles_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity # es un codigo de estado http
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+  def articles_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
 
-  root "articles#index" # definiendo un ruta raiz
+   root "articles#index"
 
-  get "articles", to: "articles#index"
+  # get "articles", to: "articles#index"
+  # get "articles/:id", to:"articles#show"
 
-  get "articles/:id", to:"articles#show"
+  # podemos cambiar esto por resources y se generaran los metodos en el controlador para nosotros
+  resources :articles
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
